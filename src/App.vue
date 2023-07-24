c5121af38e2ec327535795e906cdf8fcd1897eaa
hugofsu79 <131622641+hugofsu79@users.noreply.github.com>
2023-07-24 15:25:12 +0200

    Mon projet avec l'arborescence créer et pret pour commencer le projet

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="kinema, Découvrez notre recherche de séances cinéma et plus encore..."/>
 </template>
 
 <script>
